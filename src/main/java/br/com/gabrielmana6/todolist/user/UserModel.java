5ca62c0d2b12219adcbcdfe94bad73185180dfea
gabrielmana6 <gabrielmanasses13@gmail.com>
2023-10-12 16:00:19 -0300

    aula 01

---
+++ src/main/java/br/com/gabrielmana6/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.gabrielmana6.todolist.user;
+
+public class UserModel {
+    
+    public String name;
+    public String username;
+    public String password;
+}
---
+++ src/test/java/br/com/gabrielmana6/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmana6.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
